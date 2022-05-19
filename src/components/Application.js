a6c4f4179521396921514027ce1539ca24f5ef35
bulabd <bulabd@outlook.com>
2022-05-19 17:21:42 -0400

    Create a combined state for all the useState fcts

--- src/components/Application.js
+++ src/components/Application.js
@@ -48,12 +48,21 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [day, setDay] = useState('Monday');
-  const [days, setDays] = useState([]);
+  // const [day, setDay] = useState('Monday');
+  // const [days, setDays] = useState([]);
+  // const [appointments, setAppointments] = useState({});
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    // appointments: {}
+  });
+
+  const setDay = day => setState({...state, day});
+  const setDays = days => setState(prev => ({...prev, days}));
 
   useEffect(() => {
     axios.get('/api/days').then((response) => {
-      console.log(response);
+      // console.log(response);
       setDays(response.data);
     });
   }, []);
@@ -79,10 +88,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            // day={day}
-            // setDay={setDay}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
