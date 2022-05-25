4243abff8774b15f505b1a226dc57ee8ee735d7d
bulabd <bulabd@outlook.com>
2022-05-25 14:17:31 -0400

    Install cypress and create the first tests

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,20 @@
+describe("Navigation", () => {
+  it("should visit root", () => {
+    cy.visit("/");
+  });
+
+  // it("Should navigate to Tuesday", () => {
+  //   cy.visit('/');
+  //   cy.contains("[data-testid=day]", "Tuesday")
+  //     .click()
+  //     .should("have.class", "day-list_item--selected");
+  // });
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+  
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected")
+  });
+});
